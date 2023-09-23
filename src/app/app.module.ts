79a97a936d898c5ee9eaaac13009e04f211d31de
peter_chao <peter_chao@dongfeng.com.tw>
2023-09-23 08:55:05 +0800

    feat:[Api串接練習] 新增messages、users元件，user、messages服務 (User物件尚未建立完畢)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { MessagesService } from './messages.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private http: HttpClient,
+    private messageService :MessagesService
+  ) {}
+
+  private log(message: string) {
+    this.messageService.add(`HeroService：${message}`);
+  }
+
+  // Api Url
+  private userUrl = 'api/userdatas';
+
+  // 定義其標頭
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  // 取得使用者清單
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.userUrl).pipe(
+      tap(_ => this.log('使用者獲取中')),
+      catchError(this.handleError<User[]>('getUsers', []))
+    );
+  }
+
+  // 對應錯誤發生時的處理函式
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+}
