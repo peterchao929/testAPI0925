f8e8398cc46ada9a46bb3c7c9ac97eea3ec58af3
peter_chao <peter_chao@dongfeng.com.tw>
2023-09-25 11:45:13 +0800

    feat:[Api串接練習] 新增元件 addUser(新增使用者頁面)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { MessagesComponent } from './messages/messages.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    MessagesComponent
+    MessagesComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
