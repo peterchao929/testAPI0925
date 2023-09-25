eae68eb52ad4e3f12cbc80e509c9794b178e2bd4
peter_chao <peter_chao@dongfeng.com.tw>
2023-09-25 11:43:54 +0800

    fix:[Api串接練習] User元件名稱異常排除(與API欄位未對應)，修改部分路由(users)

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,8 +1,8 @@
 export interface User {
   id: number;
-  firstName: string;
-  lastName: string;
+  firstname: string;
+  lastname: string;
   gender: number;
   age: number;
-  registerTime: string;
+  registerDate: string;
 }
